00117105182f04b1c247378e734d78b9a08ef14d
lcappellini <lorenzo.cappellini4@edu.unifi.it>
2025-02-19 14:53:14 +0100

    Implemented command-line argument parsing

--- main.cpp
+++ main.cpp
@@ -1,75 +1,274 @@
 #include <iostream>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include "Image.h"
 #include "PNMFileHandler.h"
 #include "ImageEditor.h"
 
-int main() {
 
-    //Image * img = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\animal.ppm)");
-    //Image * img = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\animal_bytes.ppm)");
-    //Image * img = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\animal_bytescomm.ppm)");
-    Image * img = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\statue_bytes16.ppm)");
+enum class ImageEffect {
+    Sharpen,
+    GaussianBlur,
+    BrightnessChange,
+    Crop,
+    Inversion,
+    ToGrayscale,
+};
 
-    //PNMFileHandler::save_plain(img, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\animal_comm.ppm)");
+using namespace std;
 
-    cout << img->get_width() << endl;
-    cout << img->get_height() << endl;
-    cout << (int)img->get_nChannels() << endl;
-    cout << (int)img->get_bitDepth() << endl;
+void show_help() {
+    cout << "Kernel Image Processor by Lorenzo Cappellini\n";
+    cout << "Supported image formats: .pbm, .pgm, .ppm\n";
+    cout << "Supported bit depth values: 8, 16\n\n";
+    cout << "Available Arguments:\n";
+    cout << "-i <input_file>         : Specify input file path\n";
+    cout << "-o <output_file>        : Specify output file path (encoded as bytes)\n";
+    cout << "-op <output_file>       : Specify output file path (encoded as plain ascii)\n";
+    cout << "-sharpen                : Apply sharpening effect\n";
+    cout << "-blur                   : Apply Gaussian blur effect\n";
+    cout << "-brightness <factor>    : Change brightness by the given factor\n";
+    cout << "-crop <coordinates>     : Crop the image with comma-separated coordinates for the vertices: \"x0,y0,x1,y1\"\n";
+    cout << "-invert                 : Invert the colors of the image\n";
+    cout << "-grayscale              : Convert the image to grayscale\n";
+    cout << "-v                      : Enable verbose output for debugging information\n";
+    cout << "-h                      : Show this help message\n";
+}
 
+void log_error(const string& message){
+    cerr << "ERROR: " << message << endl;
+    exit(1);
+}
 
-    /*Image * grimg = ImageEditor::grayscale(img);
+int main(int argc, char *argv[]) {
+    string inputFile;
+    string outputFile;
+    bool inputFileProvided = false;
+    bool outputFileProvided = false;
+    bool outputPlain = false;
 
-    PNMFileHandler::save_plain(img, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\KernelImageProcessor\test\data\P3_16.ppm)");
-    PNMFileHandler::save(img, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\KernelImageProcessor\test\data\P6_16.ppm)");
+    bool verbose = false;
 
-    PNMFileHandler::save_plain(grimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\KernelImageProcessor\test\data\P2_16.ppm)");
-    PNMFileHandler::save(grimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\KernelImageProcessor\test\data\P5_16.ppm)");*/
+    float brightnessFactor;
+    ImageEffect imageEffect;
+    bool imageEffectProvided = false;
+    vector<int> cropCoords;
 
-    /*uint16_t *pixel = img->get_at(51, 77);
-    for (int i = 0; i < img->get_nChannels(); i++) {
-        cout << (int)pixel[i] << ",";
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+
+        if (arg == "-i") {
+            if (inputFileProvided) {
+                log_error("You can specify only 1 input file");
+            }
+            if (i + 1 >= argc){
+                log_error("Bad argument format");
+            }
+            inputFile = argv[i + 1];
+            i++;
+            inputFileProvided = true;
+
+        } else if (arg == "-o" || arg == "-op") {
+            if (outputFileProvided) {
+                log_error("You can specify only 1 output file");
+            }
+            if (i + 1 >= argc){
+                log_error("Bad argument format");
+            }
+            outputFile = argv[i + 1];
+            i++;
+            outputFileProvided = true;
+            outputPlain = arg == "-op";
+
+        } else if (arg == "-sharpen") {
+            if (imageEffectProvided) {
+                log_error("You can specify only 1 effect argument");
+            }
+            imageEffect = ImageEffect::Sharpen;
+            imageEffectProvided = true;
+
+        } else if (arg == "-blur") {
+            if (imageEffectProvided) {
+                log_error("You can specify only 1 effect argument");
+            }
+            imageEffect = ImageEffect::GaussianBlur;
+            imageEffectProvided = true;
+
+        } else if (arg == "-brightness") {
+            if (imageEffectProvided) {
+                log_error("You can specify only 1 effect argument");
+            }
+            if (i + 1 >= argc){
+                log_error("Bad argument format");
+            }
+            try {
+                brightnessFactor = stof(argv[i + 1]);
+                i++;
+            } catch (const std::invalid_argument& e) {
+                log_error("Invalid float value for argument: " + arg);
+            }
+            imageEffect = ImageEffect::BrightnessChange;
+            imageEffectProvided = true;
+
+        } else if (arg == "-crop") {
+            if (imageEffectProvided) {
+                log_error("You can specify only 1 effect argument");
+            }
+            if (i + 1 >= argc) {
+                log_error("Missing value for argument: " + arg);
+            }
+            string values = argv[i + 1];
+            stringstream ss(values);
+            string item;
+            while (std::getline(ss, item, ',')) {
+                try {
+                    cropCoords.push_back(std::stoi(item));
+                } catch (const invalid_argument& e) {
+                    log_error("Invalid number in -c argument: " + item);
+                }
+            }
+            i++;
+            imageEffect = ImageEffect::Crop;
+            imageEffectProvided = true;
+
+        } else if (arg == "-invert") {
+            if (imageEffectProvided) {
+                log_error("You can specify only 1 effect argument");
+            }
+            imageEffect = ImageEffect::Inversion;
+            imageEffectProvided = true;
+
+        } else if (arg == "-grayscale") {
+            if (imageEffectProvided) {
+                log_error("You can specify only 1 effect argument");
+            }
+            imageEffect = ImageEffect::ToGrayscale;
+            imageEffectProvided = true;
+
+        } else if (arg == "-v") {
+            verbose = true;
+
+        } else if (arg == "-h") {
+            show_help();
+            exit(1);
+
+        } else {
+            log_error("Bad argument format or unknown argument");
+        }
     }
-    cout << endl;*/
 
-    /*Kernel *ker = new Kernel(3, new float[9]{-1, -1, -1, -1, 8, -1, -1, -1, -1});
+    if (!inputFileProvided){
+        log_error("You must provide an input file");
+    }
 
-    Image * newimg = ImageEditor::convolve(img, ker, KernelMode::Crop, new uint8_t[]{255, 0, 0});
-    PNMFileHandler::save_plain(newimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\mod_animal.ppm)");
-    PNMFileHandler::save(newimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\mod_animal_bytes.ppm)");
+    if (!outputFileProvided){
+        log_error("You must provide an output file");
+    }
 
-    Image * invimg = ImageEditor::invert(img);
-    PNMFileHandler::save(invimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\inv_animal.ppm)");
+    if (!imageEffectProvided){
+        log_error("No image effect provided");
+    }
 
-    Image * brimg = ImageEditor::change_brightness(img, 0.5);
-    PNMFileHandler::save(brimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\br_animal.ppm)");
+    if (verbose) {
+        cout << "Loading file: " << inputFile << endl;
+    }
 
-    Image * grimg = ImageEditor::grayscale(img);
-    PNMFileHandler::save(grimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\gr_animal.pgm)");
+    Image *img;
+    try {
+        img = PNMFileHandler::load(inputFile);
+        if (img == nullptr) {
+            log_error("There was an error while loading the input file");
+        }
+    } catch (const std::exception& e){
+        log_error(e.what());
+        exit(1);
+    }
 
-    Image * shimg = ImageEditor::sharpen(img);
-    PNMFileHandler::save(shimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\sh_animal.ppm)");
+    if (verbose) {
+        cout << "Loaded!" << endl;
+        cout << "Properties:" << endl;
+        cout << " - Size: " << img->get_width() << ", " << img->get_height() << endl;
+        cout << " - Channels: " << (int)img->get_nChannels() << endl;
+        cout << " - BitDepth: " << (int)img->get_bitDepth() << endl;
+        cout << endl;
+    }
 
-    Image * blimg = ImageEditor::gaussian_blur3(img);
-    PNMFileHandler::save(blimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\bl_animal.ppm)");
+    Image * new_img;
 
-    Image * crimg = ImageEditor::crop(img, 14, 33, 61, 74);
-    PNMFileHandler::save(crimg, R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\cr_animal.ppm)");
+    try {
+        switch (imageEffect) {
+            case ImageEffect::Sharpen: {
+                if (verbose) {
+                    cout << "Applying sharpening...";
+                }
+                new_img = ImageEditor::sharpen(img);
+                break;
+            }
+            case ImageEffect::GaussianBlur: {
+                if (verbose) {
+                    cout << "Applying Gaussian blur...";
+                }
+                new_img = ImageEditor::gaussian_blur3(img);
+                break;
+            }
+            case ImageEffect::BrightnessChange: {
+                if (verbose) {
+                    cout << "Changing brightness...";
+                }
+                new_img = ImageEditor::change_brightness(img, brightnessFactor);
+                break;
+            }
+            case ImageEffect::Crop: {
+                if (verbose) {
+                    cout << "Cropping with coordinates: " << cropCoords.data() << "...";
+                }
+                new_img = ImageEditor::crop(img, cropCoords[0], cropCoords[1], cropCoords[2], cropCoords[3]);
+                break;
+            }
+            case ImageEffect::Inversion: {
+                if (verbose) {
+                    cout << "Applying inversion...";
+                }
+                new_img = ImageEditor::invert(img);
+                break;
+            }
+            case ImageEffect::ToGrayscale: {
+                if (verbose) {
+                    cout << "Converting to grayscale...";
+                }
+                new_img = ImageEditor::grayscale(img);
+                break;
+            }
+        }
+    } catch (const std::exception& e){
+        if (verbose)
+            cout << endl << endl;
+        log_error(e.what());
+        exit(1);
+    }
 
+    if (verbose) {
+        cout << "Done" << endl;
+    }
 
-    Image *p1 = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\tests\p1.pbm)");
-    cout << "p1 loaded" << endl;
-    Image *p2 = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\tests\p2.pgm)");
-    cout << "p2 loaded" << endl;
-    Image *p3 = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\tests\p3.ppm)");
-    cout << "p3 loaded" << endl;
-    Image *p4 = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\tests\p4.pbm)");
-    cout << "p4 loaded" << endl;
-    Image *p5 = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\tests\p5.pgm)");
-    cout << "p5 loaded" << endl;
-    Image *p6 = PNMFileHandler::load(R"(C:\Users\Lorenzo\Desktop\UNI\LAB PROG\example files\tests\p6.ppm)");
-    cout << "p6 loaded" << endl;*/
+    int result;
+    try {
+        if (outputPlain) {
+            result = PNMFileHandler::save_plain(new_img, outputFile);
+        } else {
+            result = PNMFileHandler::save(new_img, outputFile);
+        }
+    } catch (const std::exception& e) {
+        log_error(e.what());
+        exit(1);
+    }
 
-
+    if (result == 0){
+        cout << "Success!" << endl;
+    } else {
+        log_error("There was an error while saving the output file");
+    }
 
     return 0;
 }
